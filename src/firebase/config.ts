51eaaeadb776f05c57c5fa12d6a750669b349138
Zaki Mohammed <firebase.macos@gmail.com>
2025-09-25 00:04:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7501465959-64f59",
+  "appId": "1:534524538609:web:2d1d75780a834798ca8350",
+  "apiKey": "AIzaSyAS0OPy6KnMxvxtS_zavYhtuQW13_PWFYs",
+  "authDomain": "studio-7501465959-64f59.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "534524538609"
+};
