604264ebf4c4ff9372df4f49a86280177d73118c
Zaki Mohammed <firebase.macos@gmail.com>
2025-09-27 03:02:36 +0000

    الشعار الحالي جودته ضعيفه استخدم هذا
    https://i.ibb.co/QqjQT0T/1.png

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Logo(props: React.ComponentProps<typeof Image>) {
   return (
     <Image
-      src="https://i.ibb.co/xtzts1VC/Purple-and-White-Modern-Robot-Illustration-Technology-Logo.png"
+      src="https://i.ibb.co/QqjQT0T/1.png"
       alt="Logo"
       width={48}
       height={48}
